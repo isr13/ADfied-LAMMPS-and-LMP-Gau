a32a9074c30fb869a4c53e34c3df05dcc3a6f178
towara <towara@stce.rwth-aachen.de>
2025-12-01 15:06:42 +0100

    remove dco ad tool, add ad.hpp and build script

--- ADfying LAMMPS/ad_defines.h
+++ ADfying LAMMPS/ad_defines.h
@@ -2,14 +2,12 @@
 
 #ifndef AD_DEFINES_H
 #define AD_DEFINES_H
-  #define DCO_DISABLE_AVX2_WARNING
-  #define DCO_AUTO_SUPPORT
-  #include "/home/em181266/lammps_dco/dco_cpp/dcl6i34ngl/include/dco.hpp"  // change for dco path
-  DCO_ENABLE_EXPLICIT_TYPE_CAST_TO ( int )
-  DCO_ENABLE_EXPLICIT_TYPE_CAST_TO ( long )
-  typedef dco :: ga1s <double >:: type myScalar ;
+  #include "ad.hpp"
+  AD_ENABLE_TYPE_CONSTRUCTION_FROM ( int )
+  AD_ENABLE_TYPE_CONSTRUCTION_FROM ( long )
+  typedef ad::ga1s <double >::type myScalar ;
   //typedef double myScalar;
-  //typedef dco::gt1s<double>::type myScalar;
+  //typedef ad::gt1s<double>::type myScalar;
   
   inline int MY_MPI_Allreduce(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm){
     if (datatype == MPI_DOUBLE){
