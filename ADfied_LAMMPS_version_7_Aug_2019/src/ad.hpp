c86cdb970827a3dcc3b4aa43dff8a85a19431a5a
towara <towara@stce.rwth-aachen.de>
2025-12-01 14:57:07 +0100

    update licence of ad.hpp v1.2 to GPLv2+

--- ADfied_LAMMPS_version_7_Aug_2019/src/ad.hpp
+++ ADfied_LAMMPS_version_7_Aug_2019/src/ad.hpp
@@ -1,14 +1,14 @@
 /*---------------------------------------------------------------------------*\
  * Algorithmic Differentiation by Overloading in C++
-    Copyright (C) 2021 Software and Tools for Computational Engineering (STCE)
-                  RWTH Aachen University               www.stce.rwth-aachen.de
+   Copyright (C) 2021-2025 Software and Tools for Computational Engineering
+                 RWTH Aachen University               www.stce.rwth-aachen.de
 \*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*\
 License
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,8 @@ License
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program; if not, see
+    <https://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
 
@@ -25,7 +26,7 @@ License
 #ifndef AD_HPP
 #define AD_HPP
 
-#define AD_VERSION_SHORT "AD v1.2"
+#define AD_VERSION_SHORT "AD v1.2 for LAMMPS"
 
 #include <sstream>
 #include <cmath>
@@ -49,8 +50,8 @@ License
 #include <limits>
 #include <iomanip>
 
-#include <sys/mman.h>
-#include <sys/time.h>
+//#include <sys/mman.h>
+//#include <sys/time.h>
 #include <unistd.h>
 
 // for stce::timer
